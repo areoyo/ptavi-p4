c481c9b12d892f4670e6d057e7b65e1e97a1ea78
Alejandra Delgado Reoyo <areoyo@delta37.aulas.gsyc.urjc.es>
2016-10-17 17:47:43 +0200

    Mejoro sys.argv

--- client.py
+++ client.py
@@ -8,11 +8,16 @@ import socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
 
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-METODO = sys.argv[3]
-LINE = sys.argv[4]
-EXPIRES = sys.argv[5]
+#SERVER = sys.argv[1]
+#PORT = int(sys.argv[2])
+#METODO = sys.argv[3]
+#LINE = sys.argv[4]
+#EXPIRES = sys.argv[5]
+
+if len(sys.argv) != 6:
+    sys.exit("Usage: ...")
+_, SERVER, PORT, METODO, LINE, EXPIRES = sys.argv
+PORT = int(PORT)
 
 #SERVER = 'localhost' #string
 #PORT = 6001          #int
