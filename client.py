72e9659f87ecac3ebe44a878fccda4e69f60cacd
Alejandra Delgado Reoyo <areoyo@delta37.aulas.gsyc.urjc.es>
2016-10-17 17:40:02 +0200

    Añado campo Expires

--- client.py
+++ client.py
@@ -10,7 +10,9 @@ import socket
 
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = ' '.join(sys.argv[3:])
+METODO = sys.argv[3]
+LINE = sys.argv[4]
+EXPIRES = sys.argv[5]
 
 #SERVER = 'localhost' #string
 #PORT = 6001          #int
@@ -19,9 +21,12 @@ LINE = ' '.join(sys.argv[3:])
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
+    if METODO == 'register':
+        LINE = 'REGISTER sip:' + LINE + ' SIP/2.0\r\n' + 'Expires: ' + EXPIRES + '\r\n'
     print("Enviando:", LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
+#sage: client.py ip puerto register sip_address expires_value
