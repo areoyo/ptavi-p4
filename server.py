3cd15d8fc36fb53c894ea0c74dd6502200fbf1b7
Alejandra Delgado Reoyo <areoyo@gamma28.aulas.gsyc.urjc.es>
2016-10-17 10:43:24 +0200

    Impresión de IP y puerto del cliente, y pase de parametro el puerto por el que debe escuchar

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
+  #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
+import sys
 import socketserver
 
+PORT = int(sys.argv[1])
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -14,11 +15,13 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
+        print ('IP del cliente: ' + self.client_address[0])
+        print ('PUERTO del cliente: ' + str(self.client_address[1]))
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', PORT), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
