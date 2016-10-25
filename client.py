d31c5ba219058225f120af68706487e97f84b535
alejandra <a.delgadore17@gmail.com>
2016-10-25 16:41:45 +0200

    Añado mensaje a imprimir

--- client.py
+++ client.py
@@ -15,19 +15,15 @@ import socket
 #EXPIRES = sys.argv[5]
 
 if len(sys.argv) != 6:
-    sys.exit("Usage: ...")
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 _, SERVER, PORT, METODO, LINE, EXPIRES = sys.argv
 PORT = int(PORT)
-
-#SERVER = 'localhost' #string
-#PORT = 6001          #int
-#LINE = '¡Hola mundo!'#string
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     if METODO == 'register':
-        LINE = 'REGISTER sip:' + LINE + ' SIP/2.0\r\n' + 'Expires: ' + EXPIRES + '\r\n'
+        LINE = 'REGISTER sip:' + LINE + ' SIP/2.0\r\n' + 'Expires: ' + EXPIRES + '\r\n\r\n'
     print("Enviando:", LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
